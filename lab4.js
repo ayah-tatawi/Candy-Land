57fe52533314a26235a085011636e18506cb692e
aya_tantawi <aya.mf5rah@gmail.com>
2021-03-04 16:49:58 +0200

    new work

---
+++ lab4.js
@@ -0,0 +1,17 @@
+var a= prompt("your favorit color");
+
+console.log("test")
+
+if (a=="red")
+ {alert("your favorit color is red!")}
+
+ else if (a== "pink")
+
+{
+    
+ alert("your favorit color is pink!");
+}
+else
+{alert("not matched")} 
+
+document.write ("you are amazing");
